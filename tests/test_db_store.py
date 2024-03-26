d50c8598626ace02f9b431169f1329d5b8f3be90
Catalin Ene <ene.catalincostin@yahoo.com>
2024-03-26 11:48:09 +0200

    main: Added mongo driver tests and corrected driver file

--- tests/test_db_store.py
+++ tests/test_db_store.py
@@ -15,22 +15,169 @@ class TestMongoDBStore:
         }
         return pytest_document
 
+    @pytest.fixture(scope="function")
+    def sablon_document_update(self):
+        pytest_document = {
+            "name": "pytest_name_update",
+            "age": 65,
+            "active": False
+        }
+        return pytest_document
+
     def test_add_document(self, mongo_driver, sablon_document):
+        # Clean db from previous run
         mongo_driver.delete_document_by_query("sablon_db", "sablon_collection", sablon_document)
+
+        # Create document
         result = mongo_driver.add_document("sablon_db", "sablon_collection", sablon_document)
-        print(f"Added document {result}")
+        print(f" Added document {result}")
 
         assert result.get("_id") is not None
 
+        # Clean db after successful test run
         mongo_driver.delete_document_by_query("sablon_db", "sablon_collection", sablon_document)
 
     def test_get_all_documents(self, mongo_driver, sablon_document):
+        # Clean db from previous run
         mongo_driver.delete_document_by_query("sablon_db", "sablon_collection", sablon_document)
+
+        # Create document
         result = mongo_driver.add_document("sablon_db", "sablon_collection", sablon_document)
+
+        # Store created document Objectid
         document_id = result.get("_id")
+
+        # Get all documents
         result = list(mongo_driver.get_all_documents("sablon_db", "sablon_collection"))
-        print(f"{result}")
+        print(f" Get all documents: {result}")
 
         assert result[0].get("_id") == document_id
         assert result[0].get("name") == sablon_document.get("name")
+
+        # Clean db after successful test run
         mongo_driver.delete_document_by_query("sablon_db", "sablon_collection", sablon_document)
+
+    def test_get_document_by_id(self, mongo_driver, sablon_document):
+        # Clean db from previous run
+        mongo_driver.delete_document_by_query("sablon_db", "sablon_collection", sablon_document)
+
+        # Create document
+        result = mongo_driver.add_document("sablon_db", "sablon_collection", sablon_document)
+
+        # Store created document Objectid
+        document_id = result.get("_id")
+
+        # Get document by id
+        result = mongo_driver.get_document_by_id("sablon_db", "sablon_collection", document_id)
+        print(f" Get document by id: {result}")
+
+        assert result.get("_id") == document_id
+        assert result.get("name") == sablon_document.get("name")
+
+        # Clean db after successful test run
+        mongo_driver.delete_document_by_query("sablon_db", "sablon_collection", sablon_document)
+
+    def test_get_documents_by_query(self, mongo_driver, sablon_document):
+        # Clean db from previous run
+        mongo_driver.delete_document_by_query("sablon_db", "sablon_collection", sablon_document)
+
+        # Create document
+        result = mongo_driver.add_document("sablon_db", "sablon_collection", sablon_document)
+
+        # Store created document Objectid
+        document_id = result.get("_id")
+
+        # Get documents by query
+        result = mongo_driver.get_documents_by_query("sablon_db", "sablon_collection", {"name": sablon_document.get("name")})
+        print(f" Get documents by query: {result}")
+
+        assert result[0].get("_id") == document_id
+        assert result[0].get("name") == sablon_document.get("name")
+
+        # Clean db after successful test run
+        mongo_driver.delete_document_by_query("sablon_db", "sablon_collection", sablon_document)
+
+    def test_update_document(self, mongo_driver, sablon_document, sablon_document_update):
+        # Clean db from previous run
+        mongo_driver.delete_document_by_query("sablon_db", "sablon_collection", sablon_document)
+        mongo_driver.delete_document_by_query("sablon_db", "sablon_collection", sablon_document_update)
+
+        # Create document
+        result = mongo_driver.add_document("sablon_db", "sablon_collection", sablon_document)
+
+        # Store created document Objectid
+        document_id = result.get("_id")
+
+        # Update document
+        result = mongo_driver.update_document("sablon_db", "sablon_collection", document_id, sablon_document_update)
+        print(f" Update document return: {result.modified_count}")
+
+        assert result.modified_count == 1
+
+        # Get documents by query
+        result = mongo_driver.get_document_by_id("sablon_db", "sablon_collection", document_id)
+        print(f" Get document by id: {result}")
+
+        assert result.get("_id") == document_id
+        assert result.get("name") == sablon_document_update.get("name")
+        assert result.get("age") == sablon_document_update.get("age")
+        assert result.get("active") == sablon_document_update.get("active")
+
+        # Clean db after successful test run
+        mongo_driver.delete_document_by_query("sablon_db", "sablon_collection", sablon_document_update)
+
+    def test_delete_document_by_id(self, mongo_driver, sablon_document):
+        # Clean db from previous run
+        mongo_driver.delete_document_by_query("sablon_db", "sablon_collection", sablon_document)
+
+        # Create document
+        result = mongo_driver.add_document("sablon_db", "sablon_collection", sablon_document)
+        print(f" Added document {result}")
+
+        # Store created document Objectid
+        document_id = result.get("_id")
+        # print(f" Created document id stored {document_id} type {type(document_id)}")
+
+        # Delete document by id
+        result = mongo_driver.delete_document_by_id("sablon_db", "sablon_collection", document_id)
+        print(f" Deleted document {result.deleted_count}")
+
+        assert result.deleted_count == 1
+
+        # Clean db after successful test run
+        mongo_driver.delete_document_by_query("sablon_db", "sablon_collection", sablon_document)
+
+    def test_delete_document_by_query(self, mongo_driver, sablon_document):
+        # Clean db from previous run
+        mongo_driver.delete_document_by_query("sablon_db", "sablon_collection", sablon_document)
+
+        # Create document
+        result = mongo_driver.add_document("sablon_db", "sablon_collection", sablon_document)
+        print(f" Added document {result}")
+
+        # Store created document Objectid
+        document_id = result.get("_id")
+        # print(f" Created document id stored {document_id} type {type(document_id)}")
+
+        # Delete document by id
+        result = mongo_driver.delete_document_by_query("sablon_db", "sablon_collection", {"name": sablon_document.get("name")})
+        print(f" Deleted document {result.deleted_count}")
+
+        assert result.deleted_count == 1
+
+        # Clean db after successful test run
+        mongo_driver.delete_document_by_query("sablon_db", "sablon_collection", sablon_document)
+
+    def test_initialize_database(self, mongo_driver, sablon_document, sablon_document_update):
+        # Clean db from previous run
+        mongo_driver.delete_document_by_query("sablon_db", "sablon_collection", sablon_document)
+        mongo_driver.delete_document_by_query("sablon_db", "sablon_collection", sablon_document_update)
+
+        # Run initialize_database function and store the return
+        result = mongo_driver.initialize_database("sablon_db", "sablon_collection", sablon_document, sablon_document_update)
+
+        assert result is True
+
+        # Clean db after successful test run
+        mongo_driver.delete_document_by_query("sablon_db", "sablon_collection", sablon_document)
+        mongo_driver.delete_document_by_query("sablon_db", "sablon_collection", sablon_document_update)
--- utils/db_store.py
+++ utils/db_store.py
@@ -1,69 +1,97 @@
+from typing import Any, Mapping
+
 import pymongo
 from bson import ObjectId
 from pymongo import MongoClient
+from pymongo.cursor import Cursor
+from pymongo.results import UpdateResult
 
 
 class MongoDBStore:
     def __init__(self):
         self.client = MongoClient('localhost', 27017)
 
-    def add_document(self, db_name: str, db_collection: str, document: dict):
+    def add_document(self, db_name: str, db_collection: str, document: dict) -> dict:
         db = self.client[db_name]
         collection = db[db_collection]
         collection.insert_one(document)
         return document
 
-    def get_all_documents(self, db_name: str, db_collection: str):
+    def get_all_documents(self, db_name: str, db_collection: str) -> Cursor[Mapping[str, Any] | Any]:
         db = self.client[db_name]
         collection = db[db_collection]
         return collection.find()
 
-    def get_document_by_id(self, db_name: str, db_collection: str, document_id: str):
+    def get_document_by_id(self, db_name: str, db_collection: str, document_id: ObjectId) -> dict:
         db = self.client[db_name]
         collection = db[db_collection]
-        return collection.find_one({"_id": ObjectId(document_id)})
+        return collection.find_one({"_id": document_id})
 
-    def get_documents_by_query(self, db_name: str, db_collection: str, query: dict):
+    def get_documents_by_query(self, db_name: str, db_collection: str, query: dict) -> Cursor[Mapping[str, Any] | Any]:
         db = self.client[db_name]
         collection = db[db_collection]
         return collection.find(query)
 
-    def update_document(self, db_name: str, db_collection: str, document_id: str, document: dict):
+    def update_document(self, db_name: str, db_collection: str, document_id: ObjectId, document: dict) -> UpdateResult:
         db = self.client[db_name]
         collection = db[db_collection]
-        collection.update_one({"_id": ObjectId(document_id)}, {"$set": document})
-        return document
+        return collection.update_one({"_id": document_id}, {"$set": document})
 
-    def delete_document_by_id(self, db_name: str, db_collection: str, document_id: str):
+    def delete_document_by_id(self, db_name: str, db_collection: str, document_id: ObjectId):
         db = self.client[db_name]
         collection = db[db_collection]
-        return collection.delete_one({"_id": ObjectId(document_id)})
+        return collection.delete_one({"_id": document_id})
 
-    def delete_document_by_query(self, db_name: str, db_collection: str,query):
+    def delete_document_by_query(self, db_name: str, db_collection: str, query: dict):
         db = self.client[db_name]
         collection = db[db_collection]
         return collection.delete_one(query)
 
-    def initialize_database(self, db_name: str, db_collection: str):
-        db = self.client[db_name]
-        collection = db[db_collection]
+    def initialize_database(self, db_name: str, db_collection: str,
+                            test_document={"name": "test_name"},
+                            test_document_update={"name": "test_name_update"}):
+        '''
 
-        test_document = {
-            "name": "test_name"
-        }
-        test_document_update = {
-            "name": "test_name_update"
-        }
+        :param db_name:
+        :param db_collection:
+        :param test_document:
+        :param test_document_update:
+        :return: False if a step in initialization fails, and True if all steps in initialization pass
+        '''
+
         result = self.add_document(db_name, db_collection, test_document)
-        test_document_id = str(result.inserted_id)
+        test_document_id = result.get("_id")
         print(f"Document added oid: {test_document_id}")
+        if not test_document_id:
+            return False
         result = self.get_all_documents(db_name, db_collection)
         print(f"Documents found: {result}")
+        if not result[0].get("_id") == test_document_id:
+            return False
+
+        result = None # just to make sure there is no reference to previous step
         result = self.get_document_by_id(db_name, db_collection, test_document_id)
         print(f"Document found: {result}")
+        if not result.get("_id") == test_document_id:
+            return False
+
+        result = None # just to make sure there is no reference to previous step
         result = self.update_document(db_name, db_collection, test_document_id, test_document_update)
-        print(f"Documents updated: {result.modified_count}")
+        print(f"Documents updated: {result}")
+        if not result.modified_count == 1:
+            return False
+
+        result = None # just to make sure there is no reference to previous step
         result = self.get_documents_by_query(db_name, db_collection, test_document_update)
-        print(f"Document found: {result}")
-        result = self.delete_document(db_name, db_collection, test_document_id)
+        print(f"Document found: {result[0]}")
+        if not result[0].get("_id") == test_document_id:
+            return False
+
+        result = None # just to make sure there is no reference to previous step
+        result = self.delete_document_by_id(db_name, db_collection, ObjectId(test_document_id))
         print(f"Document deleted: {result.deleted_count}")
+        if not result.deleted_count == 1:
+            return False
+
+        # This return is True only if all previous steps pass
+        return True
